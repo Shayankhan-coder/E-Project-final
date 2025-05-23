64733308c8909db7130344bb5f84e7594aa5f5a4
Shayankhan-coder <shayankhan559955@gmail.com>
2025-05-23 16:00:52 +0500

    first

---
+++ javascript/index.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+    $(".btn1").click(function(){
+      $(".slide1").slideToggle();
+      
+    });
+   
+  })
+        $(document).ready(function(){
+    $(".btn2").click(function(){
+      $(".slide2").slideToggle();
+      
+    });
+   
+  })
+        $(document).ready(function(){
+    $(".btn3").click(function(){
+      $(".slide3").slideToggle();
+      
+    });
+   
+  })
+        $(document).ready(function(){
+    $(".btn4").click(function(){
+      $(".slide4").slideToggle();
+      
+    });
+   
+  })
+        $(document).ready(function(){
+    $(".btn5").click(function(){
+      $(".slide5").slideToggle();
+      
+    });
+   
+  })
+        $(document).ready(function(){
+    $(".btn6").click(function(){
+      $(".slide6").slideToggle();
+      
+    });
+   
+  })
+        $(document).ready(function(){
+    $(".btn7").click(function(){
+      $(".slide7").slideToggle();
+      
+    });
+   
+  })
+        $(document).ready(function(){
+    $(".btn8").click(function(){
+      $(".slide8").slideToggle();
+      
+    });
+   
+  })
+  
+  $(document).ready(function(){
+$(".btn9").click(function(){
+$(".slide9").slideToggle();
+
+});
+
+})
+
+
+
+
+
+function contact(){
+  alert(" Submit Succesfully.")
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
